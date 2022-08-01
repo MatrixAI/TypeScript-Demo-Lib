34f12aa95a38afaf8de2a108dfc3c661f874db65
Roger Qiu <roger.qiu@polyhack.io>
2022-08-01 20:02:45 +1000

    chore: see if jest tests are in TTY

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,3 +2,9 @@
 process.stdout._handle.setBlocking(true);
 // @ts-ignore windows
 process.stderr._handle.setBlocking(true);
+
+console.log(process.stdout.isTTY);
+console.log(process.stderr.isTTY);
+
+console.log(process.stdout._handle);
+console.log(process.stderr._handle);
