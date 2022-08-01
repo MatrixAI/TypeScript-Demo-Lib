45339e59315284b89e9e6493410e7164a433d706
Roger Qiu <roger.qiu@polyhack.io>
2022-08-01 23:53:35 +1000

    Revert "chore: turns out stdio for jest jobs are always pipes, try with force non-blocking"
    This reverts commit 7c77eb411a05b550ef9f0f60f15f3ccee3168128.

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,7 +1,7 @@
 // @ts-ignore windows
-process.stdout._handle.setBlocking(false);
+process.stdout._handle.setBlocking(true);
 // @ts-ignore windows
-process.stderr._handle.setBlocking(false);
+process.stderr._handle.setBlocking(true);
 
 console.log(process.stdout.isTTY);
 console.log(process.stderr.isTTY);
