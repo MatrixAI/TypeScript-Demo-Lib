b6b5adf98000e979f3502160b9461623f01489c7
Roger Qiu <roger.qiu@polyhack.io>
2022-08-02 00:11:40 +1000

    chore: experiment with 16.16.0

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,7 +1,9 @@
-// @ts-ignore windows
-process.stdout._handle.setBlocking(true);
-// @ts-ignore windows
-process.stderr._handle.setBlocking(true);
+// // @ts-ignore windows
+// process.stdout._handle.setBlocking(true);
+// // @ts-ignore windows
+// process.stderr._handle.setBlocking(true);
+
+console.log('SETUP');
 
 console.log(process.stdout.isTTY);
 console.log(process.stderr.isTTY);
--- tests/setupAfterEnv.ts
+++ tests/setupAfterEnv.ts
@@ -1,3 +1,11 @@
+console.log('SETUP AFTER ENV');
+console.log(process.stdout.isTTY);
+console.log(process.stderr.isTTY);
+// @ts-ignore windows
+console.log(process.stdout._handle);
+// @ts-ignore windows
+console.log(process.stderr._handle);
+
 // Default timeout per test
 // some tests may take longer in which case you should specify the timeout
 // explicitly for each test by using the third parameter of test function
